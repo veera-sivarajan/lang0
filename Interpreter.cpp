c332f81d3917edd0abf34e90a603fa2ef9d856d8
Veera <sveera.2001@gmail.com>
2021-07-25 21:41:39 -0400

    Finish implementing binary expression handlers
    The implementation is bare bones and a lot of helper methods
    are incomplete.

--- Interpreter.cpp
+++ Interpreter.cpp
@@ -12,15 +12,50 @@ void Interpreter::checkNumberOperands(const Token &oper, const std::any &left,
     throw RuntimeError{oper, "Operand must be a number."};
 }
 
+bool Interpreter::isEqual(const std::any &left, const std::any &right) {
+    
+
 std::any Interpreter::visitBinaryExpr(std::shared_ptr<Binary> expr) {
     std::any left = evaluate(expr->left);
     std::any right = evaluate(expr->right);
 
     switch (expr->oper.type) {
+
+    case TokenType::EQUAL_EQUAL:
+        return isEqual(left, right);
+
+    case TokenType::BANG_EQUAL:
+        return !isEqual(left, right);
+
+    case TokenType::GREATER:
+        checkNumberOperands(expr->oper, left, right);
+        return std::any_cast<int>(left) > std::any_cast<int>(right);
+
+    case TokenType::GREATER_EQUAL:
+        checkNumberOperands(expr->oper, left, right);
+        return std::any_cast<int>(left) >= std::any_cast<int>(right);
+
+    case TokenType::LESS:
+        checkNumberOperands(expr->oper, left, right);
+        return std::any_cast<int>(left) < std::any_cast<int>(right);
+
+    case TokenType::LESS_EQUAL:
+        checkNumberOperands(expr->oper, left, right);
+        return std::any_cast<int>(left) <= std::any_cast<int>(right);
+
     case TokenType::MINUS:
         checkNumberOperands(expr->oper, left, right);
         return std::any_cast<int>(left) - std::any_cast<int>(right);
 
+    case TokenType::PLUS:
+        if (left.type() == typeid(int) && right.type() == typeid(int)) {
+            return std::any_cast<int>(left) + std::any_cast<int>(right);
+        } 
+        else if (left.type() == typeid(string) &&
+                 right.type() == typeid(string)) {
+            return std::any_cast<string>(left) + std::any_cast<string>(right);
+        }
+        
     case TokenType::SLASH:
         checkNumberOperands(expr->oper, left, right);
         return std::any_cast<int>(left) / std::any_cast<int>(right);
@@ -28,6 +63,7 @@ std::any Interpreter::visitBinaryExpr(std::shared_ptr<Binary> expr) {
     case TokenType::STAR:
         checkNumberOperands(expr->oper, left, right);
         return std::any_cast<int>(left) * std::any_cast<int>(right);
+
     }
     
 }
--- Interpreter.hpp
+++ Interpreter.hpp
@@ -2,12 +2,17 @@
 
 # include <any>
 # include <string>
+# include "./RuntimeError.hpp"
+# include "./Token.hpp"
+# include "./Expression.hpp"
 
 class Interpreter: public ExprVisitor {
 private:
     void checkNumberOperand(const Token &oper, const std::any &operand);
     void checkNumberOperands(const Token &oper, const std::any &left,
                              const std::any &right);
+
+    bool isEqual(const std::any &left, const std::any &right);
 
 public:
     std::any visitBinaryExpr(std::shared_ptr<Binary> expr);
